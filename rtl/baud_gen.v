f01d244b7a62d6872dabcd9ed55f6a489d00c933
John Gierlach <jgierlach17@gmail.com>
2026-02-24 18:24:29 -0500

    Added the baud rate file to 9600

--- rtl/baud_gen.v
+++ rtl/baud_gen.v
@@ -1,10 +1,14 @@
 `timescale 1ns / 1ps
 
-module baud_gen()(
+module baud_gen#(parameter COUNTER_BITS = 10, parameter COUNTER_LIMIT = 651)(
     input i_clk,
     input i_reset,
     output reg o_sample_tick
     );
+
+
+    reg [COUNTER_BITS-1:0] counter;
+    reg [COUNTER_BITS-1:0] counter_buf;
 
 
     // Baud Rate = 9600
@@ -12,6 +16,18 @@ module baud_gen()(
     // Sampling Rate = 16 * Baud Rate = 153600
     // Counter Limit = 100*10^6 / 153600 = 651
 
+    always@(posedge i_clk or posedge i_reset)begin
+        if(i_reset)begin
+            counter <= 0;
+            o_sample_tick <= 0;
+        end
+        else begin
+            counter <= counter_buf;
+            o_sample_tick <= (counter == COUNTER_LIMIT-1) ? 1 : 0;
+        end
+    end
 
+    assign counter_buf = (counter == COUNTER_LIMIT-1) ? 0 : counter + 1;
+    assign tick = (counter == COUNTER_LIMIT-1) ? 1 : 0;
 
 endmodule
